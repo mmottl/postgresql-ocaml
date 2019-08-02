27d4295330e39e223ccb82fb08fd7c9205478310
Markus Mottl <markus.mottl@gmail.com>
2019-08-01 21:05:53 -0400

    Improved default Dune compilation flags

--- src/postgresql_stubs.c
+++ src/postgresql_stubs.c
@@ -528,7 +528,8 @@ static struct custom_operations result_ops = {
   custom_hash_default,
   custom_serialize_default,
   custom_deserialize_default,
-  custom_compare_ext_default
+  custom_compare_ext_default,
+  custom_fixed_length_default
 };
 
 static inline value alloc_result(PGresult *res, np_callback *cb)
